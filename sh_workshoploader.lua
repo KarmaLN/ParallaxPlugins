899c5091ab3e596fe850b3c52d9312e1f9b30213
KarmaLN <chamelphatt28@protonmail.com>
2026-02-25 21:32:45 +0100

    Add SERVER check in IncludeWorkshopAddons
    Added server check to IncludeWorkshopAddons function.

--- sh_workshoploader.lua
+++ sh_workshoploader.lua
@@ -15,33 +15,36 @@ for _, id in ipairs(EXCLUDED_WORKSHOP_IDS_LIST) do
 end
 
 local function IncludeWorkshopAddons()
-    ax.util:Print("Loading workshop addons...")
+    if SERVER then
+        ax.util:Print("Loading workshop addons...")
 
-    local total = 0
-    local skipped = 0
-    local addons = engine.GetAddons()
+        local total = 0
+        local skipped = 0
+        local addons = engine.GetAddons()
 
-    for _, v in ipairs(addons) do
-        if (v.mounted and v.wsid ~= "0") then
-            if (EXCLUDED_WORKSHOP_IDS[v.wsid]) then
-                skipped = skipped + 1
-                ax.util:Print(Color(255, 150, 0), "Skipped workshop addon: " .. v.title)
-                continue
+        for _, v in ipairs(addons) do
+            if (v.mounted and v.wsid ~= "0") then
+                if (EXCLUDED_WORKSHOP_IDS[v.wsid]) then
+                    skipped = skipped + 1
+                    ax.util:Print(Color(255, 150, 0), "Skipped workshop addon: " .. v.title)
+                    continue
+                end
+
+                total = total + 1
+                resource.AddWorkshop(v.wsid)
+                ax.util:Print("Added workshop addon: " .. v.title)
             end
-
-            total = total + 1
-            resource.AddWorkshop(v.wsid)
-            ax.util:Print("Added workshop addon: " .. v.title)
         end
-    end
 
-    ax.util:Print(
-        Color(0, 255, 0),
-        "Completed workshop addon load (" .. total .. " added, " .. skipped .. " skipped)."
-    )
+        ax.util:Print(
+            Color(0, 255, 0),
+            "Completed workshop addon load (" .. total .. " added, " .. skipped .. " skipped)."
+        )
+    end
 end
 
 function MODULE:OnSchemaLoaded()
     IncludeWorkshopAddons()
 end
+
 
